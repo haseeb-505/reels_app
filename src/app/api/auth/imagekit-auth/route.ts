22b64343360744aa7ced6e91d4e5294524cf81ef
haseeb-505 <hatahir87626@gmail.com>
2025-06-18 00:10:27 +0500

    added authentication for imagekit, added getAll vidoes and post a video endpoints

---
+++ src/app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,19 @@
+import { getUploadAuthParams } from "@imagekit/next/server"
+
+export async function GET() {
+
+    try {
+        const { token, expire, signature } = getUploadAuthParams({
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, // Never expose this on client side
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+            // expire: 30 * 60, // Optional, controls the expiry time of the token in seconds, maximum 1 hour in the future
+            // token: "random-token", // Optional, a unique token for request
+        })
+    
+        return Response.json({ token, expire, signature, publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY })
+    } catch (error) {
+        return Response.json({
+            error: "Authentication for ImageKit failed"
+        }, {status: 500})
+    }
+}
---
+++ src/app/api/video/route.ts
@@ -0,0 +1,69 @@
+import { authOptions } from "@/libs/authOptions";
+import { dbConnection } from "@/libs/dbConnect";
+import Video, { IVideo } from "@/models/Video";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await dbConnection();
+        const videos = await Video.find({})
+                        .sort({createdAt: -1}).lean();
+        if (!videos || videos.length === 0) {
+            return NextResponse.json([], {status: 200})
+        }
+
+        return NextResponse.json(videos);
+    } catch (error) {
+        return NextResponse.json(
+            {
+                error: "Failed to fetch videos"
+            }, {status: 500}
+        )
+    }
+};
+
+export async function POST(request: NextRequest) {
+    try {
+        // to authenticate the user
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json(
+                {error: "Failed to fetch videos"}, 
+                {status: 500}
+            )
+        }
+
+        // session is okay
+        await dbConnection();
+
+        const body: IVideo = await request.json();
+        if (
+            !body.title
+            || !body.videoUrl
+        ) {
+            return NextResponse.json(
+                {error: "Missing required fields"}, 
+                {status: 400}
+            )
+        }
+
+        const videoData = {
+            ...body,
+            controls: body?.controls ?? true,
+            transformation: {
+                height: 1920,
+                width: 1080,
+                quality: body.transformation?.quality ?? 100,
+            }
+        }
+        const newVideo = await Video.create(videoData)
+
+        return NextResponse.json(newVideo);
+
+    } catch (error) {
+        return NextResponse.json({
+            error: "Failed to create the video"
+        }, {status: 500})
+    }
+}
